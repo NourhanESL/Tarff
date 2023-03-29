b4aadbd25a7ed40e019adb5062d1817d171934b9
Nourhan Salah <nourhan.salah@esl.uk.com>
2023-03-29 08:59:44 +0200

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,11 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  showContent:boolean = false;
+  sentence = ' Welcome to Tarff Valley LTD.';
+  words = this.sentence.split(' ');
+  Cards=[{cardTitle:'Fertiliser',backgroundImg:'../../assets/TarrfWebsite/Image/sec1.png'},
+  {cardTitle:'Animal Health And Equipments',backgroundImg:'../../assets/TarrfWebsite/Image/sec2.png'},
+  {cardTitle:'Animal feed',backgroundImg:'../../assets/TarrfWebsite/Image/sec3.png'},
+  {cardTitle:'Fertiliser',backgroundImg:'../../assets/TarrfWebsite/Image/sec1.png'},
+  {cardTitle:'Animal Health And Equipments',backgroundImg:'../../assets/TarrfWebsite/Image/sec2.png'},
+  {cardTitle:'Animal feed',backgroundImg:'../../assets/TarrfWebsite/Image/sec3.png'},
 
+];
+
+@ViewChild('carouselTrack', { static: true }) carouselTrack: any;
+cardWidth!: number;
+numCards!: number;
+visibleCards = 4;
+currentSlide = 0;
   constructor() { }
+  ngAfterViewInit() {
+    this.cardWidth = this.carouselTrack.nativeElement.querySelector('.carousel-card').offsetWidth;
+    this.numCards = this.Cards.length;
+  }
 
+  moveCarousel(direction: string) {
+    const carouselTrack = this.carouselTrack.nativeElement;
+    if (direction === 'left' && this.currentSlide > 0) {
+      this.currentSlide--;
+      carouselTrack.style.transform = `translateX(${-this.cardWidth * this.currentSlide}px)`;
+    } else if (direction === 'right' && this.currentSlide < this.numCards - this.visibleCards) {
+      this.currentSlide++;
+      carouselTrack.style.transform = `translateX(${-this.cardWidth * this.currentSlide}px)`;
+    }
+  }
   ngOnInit(): void {
   }
 
+  toggleContent(){
+    this.showContent = !this.showContent;
+
+  }
 }
